cc5ac5bc2ed1e9e6ef42beaf17c38a2f3187ac84
Marco Antônio <ma.souza.junior@gmail.com>
2019-06-10 23:57:15 -0300

    FEA: setup docz

---
+++ doczrc.js
@@ -0,0 +1,11 @@
+export default {
+  src: './docs',
+  title: 'My Cool Project',
+  description: 'This is my awesome documentation',
+  themeConfig: {
+    colors: {
+      primary: 'tomato',
+    },
+  },
+  plugins: []
+}
