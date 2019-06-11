3c0577e8c5480b1925565955aaa722014fb5ce3c
Marco Antônio <ma.souza.junior@gmail.com>
2019-06-10 22:09:38 -0300

    REF: change imports

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,9 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Greetings from './components/Greetings'
-import TopBar from './components/TopBar'
-import theme from '../theme/muiTheme'
+import Greetings from 'app/components/Greetings'
+import TopBar from 'app/components/TopBar'
+import theme from 'app/themes/muiTheme'
 import { ThemeProvider } from '@material-ui/styles'
 
 // bootstrap component
