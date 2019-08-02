087a1b73556048ef6ddb8e8e367c48900baae215
Marco Antônio <ma.souza.junior@gmail.com>
2019-08-02 18:47:13 -0300

    FIX: change test name

--- src/app/components/TopBar/TopBar.test.js
+++ src/app/components/TopBar/TopBar.test.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme'
 import TopBar from './TopBar'
 
 describe('components/TopBar', () => {
-  it('should render Hallo', () => {
+  it('should render My awesome PWA', () => {
     const wrapper = shallow(<TopBar />)
 
     expect(wrapper.text()).toContain('My awesome PWA')
